fe357684109b9c4cf45cf34faa32283c1370f5a8
John Askew <john.askew@vouzamo.com>
2024-05-31 13:10:29 -0500

    Initial commit

---
+++ src/Vouzamo.ScoreStream.WebApp/Hubs/ScoreHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Vouzamo.ScoreStream.WebApp.Hubs
+{
+    public class ScoreHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
